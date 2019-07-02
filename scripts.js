4d111f227bbcd411a150b42d447fd134ddf7a6ec
luciaww <theluciaw@gmail.com>
2019-07-02 08:44:39 -0600

    skeleton addition

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
  function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("overlay").style.display = "block";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.body.style.backgroundColor = "white";
-}
-
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
   document.getElementById("overlay").style.display = "none";
 }
