234c21342a4a4d0494901e64742878b6a9cab5a4
luciaww <theluciaw@gmail.com>
2019-07-02 12:45:51 -0600

    adding features zz

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
- /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+ /* Set the width of the side navigation to 250px and the left margin 
+ of the page content to 250px and add a black background color to body */
  function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "400px";
+  document.getElementById("overlay").style.opacity = "1";
   document.getElementById("overlay").style.display = "block";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the width of the side navigation to 0 and the left margin of the
+ page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.body.style.backgroundColor = "white";
