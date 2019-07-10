aa69bb37e64fd93313b4fb23ace82065507658ff
luciaww <theluciaw@gmail.com>
2019-07-10 10:48:51 -0600

    Side scrolls

--- scripts.js
+++ scripts.js
@@ -53,3 +53,27 @@ function dialogClosure(){
   console.log("waited", dialog.classList);
 }
 
+
+
+/* side scrolling */
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
